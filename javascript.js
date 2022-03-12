ad748e5816601e456e34be38b6fc43c3fb09ed0b
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-12 16:07:13 -0600

    Update and rename spam.js to javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function sendMessage(title, desc, webhook) { 
+    var request = new XMLHttpRequest();
+    request.open("POST", webhook`);
+    request.setRequestHeader('Content-type', 'application/json');
+    var myEmbed = {
+        title: title,
+        description: desc,
+        color: hexToDecimal("#ff0000"),
+        footer: {
+            text: "By penguinpowers#5415"
+        }
+    }
+    var params = {
+        username: botname,
+         avatar_url: "",
+        embeds: [myEmbed]
+    }
+    request.send(JSON.stringify(params));
+    function hexToDecimal(hex) {
+        return parseInt(hex.replace("#", ""), 16)
+    }
+}
