12cad4d148b8286a19a16298c38c76a43714818f
penguinblook <100320830+penguinblook@users.noreply.github.com>
2022-03-13 18:41:22 -0400

    Update javascript.js

--- resources/javascript.js
+++ resources/javascript.js
@@ -5,7 +5,7 @@ function sendMessage(botname, title, desc, image, webhook) {
     var myEmbed = {
         title: title,
         description: desc,
-        color: hexToDecimal(#000);
+        color: hexToDecimal("#ff0000"),
         footer: {
             text: "by penguinpowers#5415"
         }
