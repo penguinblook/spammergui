1934849b8a44806bcf0c7cb8bedec86d8f13641c
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-13 17:57:55 -0500

    Update javascript.js

--- resources/javascript.js
+++ resources/javascript.js
@@ -20,3 +20,22 @@ function sendMessage(botname, title, desc, image, webhook) {
         return parseInt(hex.replace("#", ""), 16)
     }
 }
+
+function messageHandler() {
+    var i = 1;
+    let botname = document.getElementById("Botname").value;
+    let title = document.getElementById("Title").value;
+    let desc = document.getElementById("Desc").value;
+    let webhook = document.getElementById("Webhook").value; 
+    let amt = Number(document.getElementById("Num").value);
+    let img = document.getElementById('image').value;
+    sendMSG = setInterval(() => {
+        if (i <= amt) {
+            sendMessage(botname, title, desc, img, webhook)
+            i++;
+        } else {
+            clearInterval(sendMSG)
+        }
+    }, 100);
+
+}
